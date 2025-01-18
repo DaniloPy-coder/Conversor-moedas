ab4c53d1bb79d0240aee5cd45ce6dd5766d129b0
Danilo José <danilojosehapper@gmail.com>
2025-01-17 21:04:51 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ const App = () => {
       <GlobalStyles />
       <div className="container">
         <div id="img">
-          <img href="/conversor-moedas/public/imagemoeda.svg" />
+          <img src="/src/img/imagemoeda.svg" alt="Ilustração" />
         </div>
 
         <form onSubmit={handleSubmit}>
